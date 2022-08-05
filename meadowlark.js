eed48ea2770df947f4f4e30bb78fd31e32f7a0de
jbolattitorcasio <jbolattitorcasio@frba.utn.edu.ar>
2022-08-05 20:11:28 -0300

    first commit

---
+++ meadowlark.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var app = express();
+// set up handlebars view engine
+var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+var fortune = require('./lib/fortune.js');
+
+if( app.thing == null ) console.log( 'bleat!' );
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(function(req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' &&
+    req.query.test === '1';
+    next();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+ res.type('text/plain');
+ res.send('Meadowlark Travel');
+});
+app.get('/about', function(req, res) {
+        res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
+    });
+});
+app.get('/tours/hood-river', function(req, res){
+    res.render('tours/hood-river');
+});
+app.get('/tours/request-group-rate', function(req, res){
+    res.render('tours/request-group-rate');
+});
+   
+// custom 404 page
+app.use(function(req, res){
+ res.type('text/plain');
+ res.status(404);
+ res.send('404 - Not Found');
+});
+// custom 500 page
+app.use(function(err, req, res, next){
+ console.error(err.stack);
+ res.type('text/plain');
+ res.status(500);
+ res.send('500 - Server Error');
+});app.get('/', function(req, res) {
+ res.render('home');
+});
+app.get('/about', function(req, res) {
+ res.render('about');
+});
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+ res.status(404);
+ res.render('404');
+});
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+ console.error(err.stack);
+ res.status(500);
+ res.render('500');
+});
+app.listen(app.get('port'), function(){
+ console.log( 'Express started on http://localhost:' +
+ app.get('port') + '; press Ctrl-C to terminate.' );
+});
